7166e7c9775e674e72940e6680129f406b5d9d2a
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-29 04:52:22 -0600

    initial commit - project creation

---
+++ app-splitter/app-splitter/Images+Names+Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Images+Names+Colors.swift
+//  app-splitter
+//
+//  Created by Nicholas Coggin on 1/29/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+// images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let animals = UIImage(named: "3")!
+let flowers = UIImage(named: "4")!
+let stands = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, animals, flowers, stands, urban]
+
+// names
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+// Colors
+let colorsArray = [#colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1), #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)]
